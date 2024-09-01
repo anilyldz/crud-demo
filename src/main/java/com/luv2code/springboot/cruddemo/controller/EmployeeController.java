4fc46b981f2315facc569b24299ffbc1ce2a9c14
anilyildiz <anil-yildiz@outlook.com.tr>
2024-09-01 18:45:28 +0300

    Add employee operation is added

--- src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,6 +26,19 @@ public class EmployeeController {
         List<Employee> employeeList = employeeService.findAll();
         model.addAttribute("employeeList", employeeList);
         return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employeeService.save(employee);
+        return "redirect:/employees/list";
     }
 
 }
